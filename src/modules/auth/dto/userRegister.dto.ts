d6322b163e51bf24a5ec70ce7a2477c7488ab6ec
Ali Haider <alhi7896542@gmail.com>
2026-03-12 16:33:49 +0500

    apply dto validations

--- src/modules/auth/dto/userRegister.dto.ts
+++ src/modules/auth/dto/userRegister.dto.ts
@@ -1,6 +1,16 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class userRegisterDto{
+    @IsString()
     fname:string;
+
+    @IsString()
     lname:string;
+
+    @IsString()
     password:string;
+
+    @IsString()
+    @IsNotEmpty()
     email:string;
 }
