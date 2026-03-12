087a1a5c46c4c451f95398ecc0bf755313c8c8f0
Ali Haider <alhi7896542@gmail.com>
2026-03-12 15:03:59 +0500

    save data in db

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { userRegisterDto } from 'src/auth/dto/userRegister.dto';
@@ -9,12 +9,19 @@ export class UserService {
     constructor(@InjectModel(User.name) private UserModal: Model<User>) {}
 
     
-    async createUser(userRegisterDTO:userRegisterDto){
-        await this.UserModal.create({
-        fname:userRegisterDTO.fname,
-        lname:userRegisterDTO.lname,
-        email:userRegisterDTO.email,
-        password:userRegisterDTO.password,
-        })
-    }
+    async createUser(userRegisterDTO: userRegisterDto) {
+        try {
+          return await this.UserModal.create({
+            fname: userRegisterDTO.fname,
+            lname: userRegisterDTO.lname,
+            email: userRegisterDTO.email,
+            password: userRegisterDTO.password,
+          });
+        } catch (error) {
+          if (error.code === 11000 && error.keyPattern?.email) {
+            throw new ConflictException('Email already exists');
+          }
+          throw error;
+        }
+      }
 }
